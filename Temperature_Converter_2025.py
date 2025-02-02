6bda3eaba2f882b809f3f927b8227be2bdf7aed2
Thofia Thorthunder <125606870+gurlypopmagicalunicorn228@users.noreply.github.com>
2025-02-03 09:56:48 +1300

    Add files via upload
    IDK WHATS HAPPENING

---
+++ Temperature_Converter_2025.py
@@ -0,0 +1,11 @@
+#demo?
+class Converter():
+    """"""
+    "Temperature Converter tool ( °C to °F, °F to °C)"
+    """"""
+#main_routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter ()
+    root.mainloop()
